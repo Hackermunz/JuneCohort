ea354acba162c80560a7518bbb96be6fd14a6ab1
Hackermunz <137618906+Hackermunz@users.noreply.github.com>
2023-08-01 05:22:06 +0100

    Second lesson of js

---
+++ Javascript/lessons/lesson2.js
@@ -0,0 +1,42 @@
+let age = "20"
+console.log(age);
+const name = "munz"
+
+
+age = false;
+console.log(age);
+
+//name = "munz"
+console.log(typeof age);
+
+//------------------
+
+//string: anything inbetween a quotation mark is a string i.e: "munz"
+//number: a number that has nothing before and after
+//boolean: true or false statements
+//null(empty)
+//undefined: its a variable thats been set but not called.
+//object: arrays and functions
+
+let array = ["milk", "bread", "cheese", "chocolate"]
+//position/index != number of items
+
+//array.length (just means how many items in the let array)
+//array[n]= "something" will reassign item in that position 
+//array.push(item) adds item to the end of the list
+//array.pop() removes item to the end of the list
+array[3] = "fruits"
+array[4] = "juice"
+
+
+array.push(name)
+
+array.shift()
+
+array.pop()
+
+array.splice(1,3)
+
+console.log(array);
+
+
