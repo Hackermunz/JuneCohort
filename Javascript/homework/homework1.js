9bd361c1fa4f9049db9a4f32910ff06817503e3b
Hackermunz <137618906+Hackermunz@users.noreply.github.com>
2023-08-01 03:52:48 +0100

    updating homework for lesson 1 javascript

---
+++ Javascript/homework/homework1.js
@@ -0,0 +1,27 @@
+//q.1 Using string concatenation add two different strings together and print this off.
+
+console.log("Homework1.js")
+
+var location = "Dubai"
+var time = "22:00"
+
+console.log(location+" "+time);
+
+//q.2 Using variables create a variable for a number, a string. Then print each of these variables.
+
+var firstword = 63
+var secondword = "Way"
+
+console.log(firstword)
+console.log(secondword)
+
+//q.3 Using string concatenation in javascript combine 3 different variables and prints it off.
+
+var name = "Munz"
+var age = "20"
+var dob = "03"
+
+console.log('my name is', name, "i am", age , "i am born in" , dob);
+
+
+
