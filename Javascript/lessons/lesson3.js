cbc950ae9745a8a470bd7782d28c6ea9d5e4dfc8
Hackermunz <137618906+Hackermunz@users.noreply.github.com>
2023-08-10 13:00:15 +0100

    lesson3.js

---
+++ Javascript/lessons/lesson3.js
@@ -0,0 +1,27 @@
+//conditional statements 
+
+var name = "munz"
+
+// single "=" is setting variable 
+// double "==" 1 "1" (loose translation)
+// triple "===" (this is a number)1(this is a string) "1" (Means that the statement is true)   (A string has to be equal to a string and a number has to be equal to a number)
+
+
+if(name === "munz"){
+    doThis
+console.log("Hello munz welcome back my boy")
+}else{
+console.log("Who are you")
+}
+
+
+//-----------
+/*if(condition){
+    doThis
+}else if(condition){
+    doThis
+}else{
+    doThis
+}*/ 
+
+
